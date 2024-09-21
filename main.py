067e8a228f496e089963532d4bb32bc13ced54b9
hectormilla <37377168+HectorMilla@users.noreply.github.com>
2024-09-21 13:02:47 -0400

    added ablitiy draw lines using points

--- main.py
+++ main.py
@@ -1,45 +1,17 @@
+from tkinter import Tk, BOTH, Canvas
+from window import Window
+from draw import Point, Line
 
-from tkinter import TK, BOTH, Canvas
-
-
-class Window():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.__root = TK()
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    root = TK()
-    root.title = "Maze Solver"
-    
-    canvas = Canvas()
-    canvas.pack()
-    running = False
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-    
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-    
-
-    def close(self):
-        self.running = False
-    
 
 def main():
 
-    win = Window(800,800)
+    win = Window(800, 800)
+    point1, point2 = Point(10, 10), Point(200, 200)
+    line = Line(point1, point2)
+    win.draw(line, "red")
 
     win.wait_for_close()
 
 
-
-
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
---
+++ window.py
@@ -0,0 +1,29 @@
+from tkinter import Tk, BOTH, Canvas
+from draw import Point, Line
+
+
+# create a window to draw widgets
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.title = "maze solver"
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__running = False
+        self.__canvas.pack(fil=BOTH, expand=1)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
+
+    def draw(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+        
